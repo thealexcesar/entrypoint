b71ef9b70af210330383cd3d4b98688a3c6fb001
alex cesar <thealexcesar@gmail.com>
2024-08-28 21:20:21 -0300

    initial project

---
+++ src/main/java/com/entrypoint/EntrypointApplication.java
@@ -0,0 +1,13 @@
+package com.entrypoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrypointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntrypointApplication.class, args);
+    }
+
+}
